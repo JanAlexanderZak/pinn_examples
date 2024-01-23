bd3ba2680be455479676c6a62a1fc7e740f650e7
Jan Alexander Zak <janalexzak@gmail.com>
2024-01-23 17:06:38 +0100

    refactor: working hyperparameters for 2 edges

--- src/continuous_time/heat_eq_2d/executor.py
+++ src/continuous_time/heat_eq_2d/executor.py
@@ -9,6 +9,7 @@ from src.continuous_time.heat_eq_2d.model import HeatEq2DPINNRegressor
 from src.continuous_time.heat_eq_2d.data_module import HeatEq2DPINNDataModule
 from src.continuous_time.heat_eq_2d.generate_dataset import ALPHA
 
+
 def main(epochs):
     args = DeepLearningArguments(
         seed=6020,
@@ -28,14 +29,14 @@ def main(epochs):
         "layer_initialization": torch.nn.init.xavier_uniform_, #torch.nn.init.xavier_normal_
         "optimizer": torch.optim.Adam,
         "scheduler": torch.optim.lr_scheduler.ReduceLROnPlateau,
-        "scheduler_patience": 100,
+        "scheduler_patience": 20,
         "weight_decay": 0,
         "scheduler_monitor": "train_loss",
-        "learning_rate": 1e-4,
+        "learning_rate": 1e-3,
         "loss_BC_param": 1,
         "loss_PDE_param": 1,
-        "num_hidden_layers": 8,
-        "size_hidden_layers": 20,
+        "num_hidden_layers": 4,
+        "size_hidden_layers": 50,
         "dropout": False,
         "dropout_p": 0.1,
         "batch_normalization": False,
@@ -58,11 +59,11 @@ def main(epochs):
         break
 
     # Callbacks
-    # logger = pl.loggers.TensorBoardLogger(
-    #     save_dir="",
-    #     name=f"{ISO_DATE}_drop{hyper_parameters['dropout']}_bn{hyper_parameters['batch_normalization']}_dataloss{hyper_parameters['loss_data_param']}",
-    # )
-    # early_stopping = pl.callbacks.EarlyStopping("train_param_mu", patience=3000, verbose=True,)
+    logger = pl.loggers.TensorBoardLogger(
+        save_dir="",
+        name=f"{hyper_parameters['learning_rate']}_{hyper_parameters['num_hidden_layers']}_{hyper_parameters['size_hidden_layers']}",
+    )
+    early_stopping = pl.callbacks.EarlyStopping("train_loss", patience=1000, verbose=True,)
     model_summary = pl.callbacks.ModelSummary(max_depth=1)
 
     model = HeatEq2DPINNRegressor(
@@ -75,7 +76,7 @@ def main(epochs):
     model.hparams.update(data_module.hparams)
     
     trainer = pl.Trainer(
-        #callbacks=[checkpoint_every_n_steps],
+        callbacks=[early_stopping, model_summary],
         max_epochs=args.max_epochs,
         sync_batchnorm=args.sync_batchnorm,
         min_epochs=args.min_epochs,
@@ -96,4 +97,4 @@ def main(epochs):
 
 
 if __name__ == "__main__":
-    main(20000)
+    main(20002)
